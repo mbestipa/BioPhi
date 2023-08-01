a979105ab923a40dc8eb47339f6e56db2ddf3123
mbestipa <mbest@ipatherapeutics.com>
2023-08-01 16:54:57 +0200

    add types for common/utils/resources.py

--- biophi/common/utils/resources.py
+++ biophi/common/utils/resources.py
@@ -1,5 +1,5 @@
 import os
 import biophi
 
-def get_resource_path(name, module):
+def get_resource_path(name: str, module: str) -> str:
     return os.path.join(os.path.dirname(biophi.__file__), module, 'resources', name)
